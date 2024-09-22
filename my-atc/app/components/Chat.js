20bce58a55a9eb6c0bf11e6c31a6631d6cc26b63
Deo-Ekodi <deoekodi@gmail.com>
2024-09-22 13:54:21 -0800

    url update

--- my-atc/app/components/Chat.js
+++ my-atc/app/components/Chat.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('https://your-app-name.vercel.app'); // Update with your deployed URL
+const socket = io('https://my-atc.vercel.app'); // Update with your deployed URL
 
 const Chat = () => {
   const [message, setMessage] = useState('');
